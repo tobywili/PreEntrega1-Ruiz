b78f6412013198233f95ce0dee77f535811b8c2b
tobywili <ruizwilians0@gmail.com>
2025-03-26 19:38:48 -0300

    modifi en img

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded"), function () 
     const hamburger = document.getElementById("hamburger");
     const navLinks = document.getElementById("nav-links");
 
     hamburger.addEventListener("click", () => {
         navLinks.classList.toggle("active"); // Agrega o quita la clase 'active'
     });
-});
+    
